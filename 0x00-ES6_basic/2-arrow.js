87ee65b20b8f1e17a8c8fdc825524353c85fcd70
MerVitz <amakaluvitalis202@gmail.com>
2024-10-14 08:17:54 +0000

    Arrow function

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,8 @@
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => { // Using arrow function
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
