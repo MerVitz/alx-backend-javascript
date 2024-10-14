c7c54308a32b072b3f17c7e4fbb64f8ce8b0fa98
MerVitz <amakaluvitalis202@gmail.com>
2024-10-14 08:05:59 +0000

    mit
    Fixed the ES6  issue

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false; // Changed from var to let for block scope
-  const task2 = true; // Changed from var to let for block scope
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // Block-scoped variable
-    const task2 = false; // Block-scoped variable
+    task = true;
+    task2 = false;
   }
 
-  return [task, task2]; // Returns the outer task and task2
+  return [task, task2];
 }
