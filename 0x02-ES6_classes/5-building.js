9a079b8be906e53d523f1937a3c44f3375da45fe
MerVitz <amakaluvitalis202@gmail.com>
2024-11-02 05:05:55 +0000

    Morning

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+// 5-building.js
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') throw new TypeError('Sqft must be a number');
+    this._sqft = sqft;
+
+    if (new.target === Building) {
+      throw new TypeError('Cannot instantiate Building directly');
+    }
+  }
+
+  get sqft() { return this._sqft; }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,14 @@
+// 8-hbtn_class.js
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') throw new TypeError('Size must be a number');
+    if (typeof location !== 'string') throw new TypeError('Location must be a string');
+
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() { return this._size; }
+  toString() { return this._location; }
+}
+
