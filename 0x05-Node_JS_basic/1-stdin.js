384dd28752152fa95e594725f0e3b1b06c58bfea
MerVitz <amakaluvitalis202@gmail.com>
2024-12-31 07:22:34 +0300

    Calander

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,10 @@
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Handle the input via stdin (interactive or piped)
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
 });
 
-// Handle the end of input (when the user ends the input or the stream closes)
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
