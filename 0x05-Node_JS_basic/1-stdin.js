405d33b8d4a5611bc4f16717e4e3898f0d6a5326
MerVitz <amakaluvitalis202@gmail.com>
2025-01-29 08:51:04 +0300

    Implementing the pipped too

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,9 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to ALX, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
+  process.stdin.end(); // This ensures `end` event is triggered when input is piped
 });
 
 process.stdin.on('end', () => {
